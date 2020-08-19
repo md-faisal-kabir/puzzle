dfa57cccde155f4e092e35df1ad2f2a720a48754
Faisal Kabir <Rayadmin@AMC.local>
2020-08-19 12:00:43 -0700

    4x4, 8x8, 12x12 image splitting

--- src/components/puzzle.jsx
+++ src/components/puzzle.jsx
@@ -1,30 +1,62 @@
 import React, { Component } from 'react';
-import mountain from '../image/mountain.jpg'
+import mountain from "../image/mountain.jpg"
+
 
 class ImageSplit extends Component {
     state ={
         puzzleTiles:[],
+        imageHeight: 716,
+        imageWidth : 1080
+        // imageUrl: "../image/mountain.jpg"
         
     }
 
-    splitter = () => {
+    splitter = (tile) => {
         const canvas = this.refs.myCanvas;
         const ctx = canvas.getContext("2d");
         const img = this.refs.mountain;
-        console.log(img.height);
+
+        let tileWidth = parseInt(this.state.imageWidth / tile);
+        let tileHeight = parseInt(this.state.imageHeight /tile);
+
+        console.log(tileWidth);
+        console.log(tileHeight);
+        console.log(tile);
+        ctx.clearRect(0,0,this.state.imageWidth,this.state.imageHeight);
+
         
-        ctx.drawImage(img, 0, 0, 270, 270, 0, 0,120,120);
-        ctx.drawImage(img, 270, 0, 270 , 270, 130, 0,120,120);
-        ctx.drawImage(img, 540, 0, 270 , 270, 260, 0,120,120);
-        ctx.drawImage(img, 810, 0, 270 , 270, 390, 0,120,120);
-        ctx.drawImage(img, 0, 270, 270, 270, 0, 130,120,120);
-        ctx.drawImage(img, 270, 270, 270 , 270, 130, 130,120,120);
-        ctx.drawImage(img, 540, 270, 270 , 270, 260, 130,120,120);
-        ctx.drawImage(img, 810, 270, 270 , 270, 390, 130,120,120);
-        ctx.drawImage(img, 0, 540, 270, 270, 0, 260,120,120);
-        ctx.drawImage(img, 270, 540, 270 , 270, 130, 260,120,120);
-        ctx.drawImage(img, 540, 540, 270 , 270, 260, 260,120,120);
-        ctx.drawImage(img, 810, 540, 270 , 270, 390, 260,120,120);
+        let count = 0;
+
+        for (let y = 0, j = 0; count < tile; y=y+tileHeight, j= j+tileHeight/2 +10){
+            let countTwo = 0;
+            console.log("loop")
+            for (let x = 0, i = 0; countTwo < tile; x=x+tileWidth,i=i+tileWidth/2 +10){
+                ctx.drawImage(img, x,  y,  tileWidth, tileHeight, i, j, tileWidth/2, tileHeight/2);
+                console.log(x, y, i, j);
+                countTwo++;
+            }
+            count++;
+        }
+
+
+
+        // ctx.drawImage(img,         0,          0,  tileWidth, tileHeight,                     0,                0, tileWidth/2, tileHeight/2);
+        // ctx.drawImage(img, tileWidth,          0,  tileWidth, tileHeight,       tileWidth/2 +10,                0, tileWidth/2, tileHeight/2);
+        // ctx.drawImage(img,         0, tileHeight,  tileWidth, tileHeight,                     0, tileHeight/2 +10, tileWidth/2, tileHeight/2);
+        // ctx.drawImage(img, tileWidth, tileHeight,  tileWidth, tileHeight,       tileWidth/2 +10, tileHeight/2 +10, tileWidth/2, tileHeight/2); 
+        // ctx.drawImage(img, 540, 0, tileWidth, tileHeight ,       2*tileWidth/2 +20, 0, tileWidth/2, tileHeight/2);
+        // ctx.drawImage(img, 810, 0, tileWidth, tileHeight ,       3*tileWidth/2 +30, 0, tileWidth/2, tileHeight/2);
+        
+
+        // ctx.drawImage(img, 0, 270, 270, 270, 0, 130, 120, 120);
+        // ctx.drawImage(img, 270, 270, 270 , 270, 390, 130, 120, 120);
+        // ctx.drawImage(img, 540, 270, 270 , 270, 260, 130, 120, 120);
+        // ctx.drawImage(img, 810, 270, 270 , 270, 130, 130, 120, 120);
+
+        // ctx.drawImage(img, 0, 540, 270, 270, 260, 260, 120, 120);
+        // ctx.drawImage(img, 270, 540, 270 , 270, 390, 260,120,120);
+        // ctx.drawImage(img, 540, 540, 270 , 270, 0, 260,120,120);
+        // ctx.drawImage(img, 810, 540, 270 , 270, 130, 260,120,120);
         let im = canvas.toDataURL();
         let a = document.createElement('a');
         a.href = im;
@@ -36,15 +68,9 @@ class ImageSplit extends Component {
         // ctx.drawImage(img, 500, 0, 500, 500, 210, 0,200,200);
         // ctx.drawImage(img, 0, 500, 500, 500, 0, 210 ,200,200);
         // ctx.drawImage(img, 500, 500, 500, 500, 210, 210 ,200,200);
-                  // ctx.drawImage(img, 120, 0, 240, 480, 130, 0, 240, 480);
-                
-               
-            }
-          
-        //   ctx.font = "40px Courier";
-        //   ctx.fillText(this.props.text, 210, 75);
-
-
+        // ctx.drawImage(img, 120, 0, 240, 480, 130, 0, 240, 480);
+        }
+                     
       
 
 
@@ -54,13 +80,16 @@ class ImageSplit extends Component {
             <React.Fragment>
                 <h1 className="text-center display-1"> Puzzle </h1>
                 <div className="row">
+                    <div className="col-md-2 ml-2"></div>
                 <div className="col-md-3 m-5">
                     <img ref="mountain" src = { mountain } alt = " " width="500" />
-                    <button className ="btn btn-secondary mt-3" onClick = {this.splitter }> Split</button>
+                    <button className ="btn btn-secondary m-3" onClick = {( )=>this.splitter(4) }> 4x4</button>
+                    <button className ="btn btn-primary m-3"   onClick = {( )=>this.splitter(8) }> 8x8</button>
+                    <button className ="btn btn-warning m-3"   onClick = {( )=>this.splitter(12)}> 12x12</button>
                 </div>
 
-                <div className="col-md-5 m-5">
-                    <canvas ref="myCanvas" width="550" height="550"></canvas>
+                <div className="col-md-3 m-5">
+                    <canvas ref="myCanvas" width="1000" height="550"></canvas>
                 </div>
                 
             </div>
